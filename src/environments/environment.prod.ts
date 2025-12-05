d47d80628be504898f75f11d0fe66353b6915403
Krx-21 <6733007821@student.chula.ac.th>
2025-12-05 17:09:25 +0700

    chore: add environment files for production build and update gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCUyrF-oeSZ4BmF86X_THHR-hi1szOiacw",
+    authDomain: "ems-cedt.firebaseapp.com",
+    projectId: "ems-cedt",
+    storageBucket: "ems-cedt.firebasestorage.app",
+    messagingSenderId: "1097953573394",
+    appId: "1:1097953573394:web:41509ca9f1ac627cd276cb",
+    measurementId: "G-CB9KMC0ZE0",
+    databaseURL: "https://ems-cedt-default-rtdb.asia-southeast1.firebasedatabase.app"
+  }
+};
